4e7f4708e55668ae53fbee297052167b9d04f3c7
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-07-31 20:03:13 -0400

    Cleanup environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,9 @@
 const gulp = require('gulp');
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
-const browserSync = require('browser-sync').create();
 
-gulp.task('dev', async () => {
+gulp.task('dev', () => {
   gulp.watch('src/*', ['build']);
-});
-
-gulp.task('dev:demo', async () => {
-  browserSync.init({
-    server: {
-      baseDir: './example/',
-      directory: true,
-    },
-    startPath: '/index.html',
-  });
-
-  gulp.watch('example/*.html').on('change', browserSync.reload);
-  gulp.watch('example/*.css').on('change', browserSync.reload);
-  gulp.watch('src/*', ['build']).on('change', browserSync.reload);
 });
 
 gulp.task('build:umd', async () => {
